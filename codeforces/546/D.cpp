98b2b0fcc5f858f86055e0d75d15dfb35947ce21
Muhammad Al-Halaby <moj1323@gmail.com>
2021-01-23 19:16:00 +0200

    Add solution for problem `D - Soldier and Number Game` Link: https://codeforces.com/contest/546/problem/D Tags: constructive algorithms, dp, math, number theory, *1700 Ref: https://codeforces.com/contest/546/submission/105171314

--- codeforces/546/D.cpp
+++ codeforces/546/D.cpp
@@ -11,20 +11,24 @@ const int N = 5e6 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
 const ll llOO = 0x3f3f3f3f3f3f3f3f;
 
 vector<bool> isPrime(N, true);
-vector<int> smallestPrimeFactor(N, 1);
 vector<ll> factorsCount(N, 0);
 
 void sieve(){
     isPrime[0] = isPrime[1] = 0;
     /* This loop could be optimized */
-    for(ll i = 2; i < N; i++) {
+    for(int i = 2; i < N; i++) {
         if (isPrime[i]) {
-            smallestPrimeFactor[i] = i;
-            for (ll j = i * i; j < N; j += i) {
+
+            /* This loop could be optimized */
+            for (int j = i; j < N; j += i) {
                 if(j != i)
                     isPrime[j] = false;
+
                 /* Keep it only if you need it */
-                smallestPrimeFactor[j] = i;
+                int cpy = j, cnt = 0;
+                while (cpy % i == 0) cpy /= i, cnt++;
+
+                factorsCount[j] += cnt;
             }
         }
     }
@@ -35,10 +39,8 @@ int main() {
     cin.sync_with_stdio(0);
 
     sieve();
-    for(int i = 2;i < N;i++)
-        factorsCount[i] += factorsCount[i / smallestPrimeFactor[i]] + 1;
-
-    partial_sum(factorsCount.begin(), factorsCount.end(), factorsCount.begin());
+    for(int i = 3;i < N;i++)
+        factorsCount[i] += factorsCount[i - 1];
 
     int t;  cin >> t;
     while(t--){
