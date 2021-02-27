014679122d1a85c37659ac4d3c54304f7a726b98
Muhammad Al-Halaby <moj1323@gmail.com>
2021-02-27 14:12:00 +0200

    Add solution for problem `C - Unexpressed` Link: https://atcoder.jp/contests/abc193/tasks/abc193_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc193/submissions/20529353

---
+++ atcoder/abc193/C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    ll n;   cin >> n;
+    set<ll> s;
+    for (ll i = 2; i * i <= n; i++) {
+        ll x = i * i;
+        while(x <= n){
+            s.insert(x);
+            x *= i;
+        }
+    }
+
+    cout << n - s.size();
+}
