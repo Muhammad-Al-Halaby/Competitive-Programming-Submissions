7b22641eb2cfda19b1ee091794fcd73ebbf68560
Muhammad Al-Halaby <moj1323@gmail.com>
2021-03-20 14:04:00 +0200

    Add solution for problem `A - Difference Max` Link: https://atcoder.jp/contests/abc196/tasks/abc196_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc196/submissions/21067879

---
+++ atcoder/abc196/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    cout << b - c;
+}
