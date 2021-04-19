2c3145369f8de9bcfe7f9cffcc674fd0bd29b45b
Muhammad Al-Halaby <moj1323@gmail.com>
2021-04-19 16:31:00 +0200

    Add solution for problem `B - AND 0, Sum Big` Link: https://codeforces.com/contest/1514/problem/B Tags: bitmasks, combinatorics, math, *1200 Ref: https://codeforces.com/contest/1514/submission/113516436

---
+++ codeforces/1514/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+ll power(ll b, ll p, ll mod){
+    if(p == 0)return 1;
+    ll res = power(b,p/2, mod);
+    res *= res;
+    res %= mod;
+    if(p & 1)res *= b;
+    return res % mod;
+}
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int t;  cin >> t;
+    while(t--){
+        ll n, k;   cin >> n >> k;
+
+        cout << power(n, k, modulo) << endl;
+    }
+
+}
