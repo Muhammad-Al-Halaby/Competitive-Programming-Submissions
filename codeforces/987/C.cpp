43e1c88f76e0a9297bf9e9ab59e1c4ac6fda1c88
Muhammad Al-Halaby <moj1323@gmail.com>
2021-09-14 13:11:00 +0200

    Add solution for problem `C - Three displays` Link: https://codeforces.com/contest/987/problem/C Tags: brute force, dp, implementation, *1400 Ref: https://codeforces.com/contest/987/submission/128810100

--- codeforces/987/C.cpp
+++ codeforces/987/C.cpp
@@ -10,31 +10,49 @@ typedef long long ll;
 const int N = 3e3 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
 const ll llOO = 0x3f3f3f3f3f3f3f3f;
 
-int n, s[N], c[N], bestCost[N];
+int n, s[N], c[N], last;
+
+ll dp[N][N];
+
+ll solve(int i, int prev){
+    if(prev != last)
+        return 0;
+
+    if(i == n)
+        return llOO;
+
+    ll &ret = dp[i][prev];
+    if(~ret)    return ret;
+
+    ret = solve(i + 1, prev);
+
+    if(s[i] > s[prev])
+        ret = min(ret, c[i] + solve(i + 1, i));
+
+    return ret;
+}
 
 int main() {
     cin.tie(0);
     cin.sync_with_stdio(0);
 
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for(int i = 0;i < n;i++)
         cin >> s[i];
 
-    for (int i = 0; i < n; i++)
+    for(int i = 0;i < n;i++)
         cin >> c[i];
 
-    memset(bestCost, OO, sizeof bestCost);
+    memset(dp, -1, sizeof dp);
 
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            if (s[j] > s[i])
-                bestCost[i] = min(bestCost[i], c[j]);
+    ll ans = llOO;
+    for(int i = 0;i < n;i++){
+        for(int j = i + 1;j < n;j++){
+            last = j;
+            if(s[i] < s[j])
+                ans = min(ans, c[i] + c[j] + solve(j + 1, j));
+        }
+    }
 
-    int ans = OO;
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            if (s[i] < s[j])
-                ans = min(ans, c[i] + c[j] + bestCost[j]);
-
-    cout << (ans >= OO ? -1 : ans);
+    cout << (ans >= llOO ? -1 : ans);
 }
