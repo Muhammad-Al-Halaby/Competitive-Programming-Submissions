68640c7e327e3a6296f27b109e90ff8c2eaebfd1
Muhammad Al-Halaby <moj1323@gmail.com>
2021-09-18 14:01:00 +0200

    Add solution for problem `A - AtCoder Quiz 2` Link: https://atcoder.jp/contests/abc219/tasks/abc219_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc219/submissions/25920243

---
+++ atcoder/abc219/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int x;  cin >> x;
+
+    if(x < 40)
+        cout << 40 - x;
+    else if(x < 70)
+        cout << 70 - x;
+    else if(x < 90)
+        cout << 90 - x;
+    else
+        cout << "expert";
+}
