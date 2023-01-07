c263078be25a7e9ac45ba2c3feb07cfe792d17ab
Muhammad Al-Halaby <moj1323@gmail.com>
2023-01-07 21:12:00 +0200

    Add solution for problem `D - Happy New Year 2023` Link: https://atcoder.jp/contests/abc284/tasks/abc284_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc284/submissions/37861206

---
+++ atcoder/abc284/D.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+
+        for (int i = 2; i < 1e9; i++) {
+            if (n % i == 0) {
+                if((n / i) % i == 0){
+                    cout << i << " " << n / i / i << '\n';
+                    break;
+                }
+                else{
+                    ll l = i + 1, r = 1e10;
+                    ll rem = n / i;
+                    while(l <= r){
+                        ll mid = (l + r) >> 1;
+                        if(rem / mid >= mid)
+                            l = mid + 1;
+                        else
+                            r = mid - 1;
+                    }
+                    cout << r << " " << i << '\n';
+                    break;
+                }
+            }
+        }
+    }
+}
