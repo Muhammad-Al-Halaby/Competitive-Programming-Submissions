62464ceb214a929b186958b98f7e54f72c09e56f
Muhammad Al-Halaby <moj1323@gmail.com>
2023-02-25 03:05:00 +0200

    Add solution for problem `E - Negatives and Positives` Link: https://codeforces.com/contest/1791/problem/E Tags: dp, greedy, sortings, *1100 Ref: https://codeforces.com/contest/1791/submission/194856985

---
+++ codeforces/1791/E.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int mn = OO, neg = 0;
+        ll sum = 0;
+        for(int i = 0;i < n;i++){
+            int x;  cin >> x;
+            neg += x < 0;
+            sum += abs(x);
+            mn = min(mn, abs(x));
+        }
+
+        if(neg & 1)
+            sum -= 2 * mn;
+
+        cout << sum << '\n';
+    }
+}
