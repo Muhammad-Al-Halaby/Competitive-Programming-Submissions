a31b0db5bb86fb41bf415be0c30615c5deeed97a
Muhammad Al-Halaby <moj1323@gmail.com>
2023-02-25 03:43:00 +0200

    Add solution for problem `G1 - Teleporters (Easy Version)` Link: https://codeforces.com/contest/1791/problem/G1 Tags: greedy, sortings, *1100 Ref: https://codeforces.com/contest/1791/submission/194858134

---
+++ codeforces/1791/G1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int t;  cin >> t;
+    while(t--){
+        int n, c;   cin >> n >> c;
+
+        ll a[n];
+        for(int i = 0;i < n;i++){
+            cin >> a[i];
+            a[i] += i + 1;
+        }
+
+        sort(a, a + n);
+
+        int ans = 0;
+        for(int i = 0;i < n;i++){
+            if(c >= a[i]) {
+                ans++;
+                c -= a[i];
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+}
