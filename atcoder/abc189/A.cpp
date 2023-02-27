f4085604f242987b77c6953170ee76b76d33680d
Muhammad Al-Halaby <moj1323@gmail.com>
2023-02-27 17:11:00 +0200

    Add solution for problem `A - Slot` Link: https://atcoder.jp/contests/abc189/tasks/abc189_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc189/submissions/39298136

---
+++ atcoder/abc189/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    char c1, c2, c3;
+    cin >> c1 >> c2 >> c3;
+
+    if(c1 == c2 && c2 == c3)
+        cout << "Won";
+    else
+        cout << "Lost";
+}
