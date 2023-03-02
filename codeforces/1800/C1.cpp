0cdfd1fbfd54771bce571645a6ed17708dc18e66
Muhammad Al-Halaby <moj1323@gmail.com>
2023-03-02 17:17:00 +0200

    Add solution for problem `C1 - Powering the Hero (easy version)` Link: https://codeforces.com/contest/1800/problem/C1 Tags: data structures, greedy Ref: https://codeforces.com/contest/1800/submission/195629089

---
+++ codeforces/1800/C1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        ll ans = 0;
+        multiset<int> s;
+        for(int i = 0;i < n;i++){
+            int x;  cin >> x;
+            if(x > 0) {
+                s.insert(x);
+            }
+            else{
+                if(s.size()) {
+                    auto it = --s.end();
+                    ans += *it;
+                    s.erase(it);
+                }
+            }
+
+        }
+
+        cout << ans << '\n';
+    }
+}
