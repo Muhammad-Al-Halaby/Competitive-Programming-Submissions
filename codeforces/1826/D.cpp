2a9a32fd88b73837810f1d8174c56136733adc6e
Muhammad Al-Halaby <moj1323@gmail.com>
2023-05-22 06:00:00 +0300

    Add solution for problem `D - Running Miles` Link: https://codeforces.com/contest/1826/problem/D Tags: brute force, dp, greedy, *1700 Ref: https://codeforces.com/contest/1826/submission/206789738

--- codeforces/1826/D.cpp
+++ codeforces/1826/D.cpp
@@ -2,41 +2,44 @@
 
 using namespace std;
 
-const int N = 1e5 + 9, OO = 0x3f3f3f3f;
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
 
-int n, a[N];
-int dp[N][3];
+typedef long long ll;
 
-int solve() {
-    dp[0][0] = a[0] + 1;
-    dp[0][1] = a[0];
-    dp[0][2] = -OO;
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
 
-    for (int i = 1; i < n; i++) {
-        dp[i][0] = max(dp[i - 1][0], a[i] + (i + 1));
-        dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + a[i]);
-        dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] + a[i] - (i + 1));
-    }
-
-    return dp[n - 1][2];
-}
 
 int main() {
-    ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
+    cin.sync_with_stdio(0);
 
     int t;
     cin >> t;
-    while (t--) {
+    while(t--){
+        int n;
         cin >> n;
 
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
+        int start[n], middle[n], end[n];
+        for(int i = 0;i < n;i++){
+            cin >> start[i];
+            middle[i] = start[i];
+            end[i] = start[i] - (i + 1);
+            start[i] += (i + 1);
         }
 
-        cout << solve() << '\n';
-    }
+        int mx = end[n - 1], max_match[n];
+        max_match[n - 1] = -OO;
+        for(int i = n - 2;i >= 1;i--){
+            max_match[i] = max(max_match[i + 1], middle[i] + mx);
+            mx = max(mx, end[i]);
+        }
 
-    return 0;
+        int ans = -OO;
+        for(int i = 0;i < n - 2;i++)
+            ans = max(ans, start[i] + max_match[i + 1]);
+
+        cout << ans << '\n';
+    }
 }
