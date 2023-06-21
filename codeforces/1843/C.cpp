0119e8ae6a85fdfce2648ff66636aba2ca36699c
Muhammad Al-Halaby <moj1323@gmail.com>
2023-06-21 20:40:00 +0300

    Add solution for problem `C - Sum in Binary Tree` Link: https://codeforces.com/contest/1843/problem/C Tags: bitmasks, math, trees Ref: https://codeforces.com/contest/1843/submission/210576991

---
+++ codeforces/1843/C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define modulo ll (1e9 + 7)
+#define neig(a, u, e, v) for(int v, e = (a).head[u] ; ~e and (v = (a).to[e], 1) ; e = (a).nxt[e])
+
+typedef long long ll;
+
+const int N = 2e5 + 9, M = 1e6 + 9, OO = 0x3f3f3f3f;
+const ll llOO = 0x3f3f3f3f3f3f3f3f;
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll n;
+        cin >> n;
+
+        ll ans = 0;
+        while (n >= 1) {
+            ans += n;
+            n /= 2;
+        }
+
+        cout << ans << '\n';
+    }
+}
